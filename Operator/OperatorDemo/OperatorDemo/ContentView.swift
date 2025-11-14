d5f4fbcfbbe937a3e524ebe475a128bdb85fcb94
Swapnil Magar <magarswapnil896@gmail.com>
2025-11-14 22:15:58 +0530

    Operators demo

---
+++ Operator/OperatorDemo/OperatorDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OperatorDemo
+//
+//  Created by Pranav Manikpure on 14/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
