59f9d57d93fb5abdaf84d56e2777e373f4c26995
Swapnil Magar <magarswapnil896@gmail.com>
2025-11-23 13:31:42 +0530

    added parameter for custom operators

---
+++ Operator/CustomOperators.playground/Pages/with_parameters.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Combine
+
+print("============================================")
+extension Publisher where Output == Int {
+    
+    func filterNumberGreaterThan(_ value: Int) -> AnyPublisher<Int, Failure> {
+        return self.filter { $0 > value }
+            .eraseToAnyPublisher()
+    }
+}
+
+let publisher = (1...10).publisher
+let filteredPublisher = publisher.filterNumberGreaterThan(5)
+let cancellable = filteredPublisher.sink { value in
+    print(value)
+}
